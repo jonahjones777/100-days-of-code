689b429639a97b1f51b44613ddccd8eceb3110d4
jjones <jonah.jones@maine.edu>
2019-01-13 10:41:53 -0500

    adding in Identity pool

--- notes/quickstart.sh
+++ notes/quickstart.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-region=us-east-1
+# region=us-east-1
 
 
-aws cloudformation create-stack \
-    --stack-name notes-backend \
-    --template-url https://s3.amazonaws.com/notes-artifactstores3location-ap8i1gybzyuw/backend.yml \
-    --parameters ParameterKey=S3Artifact,ParameterValue=notes-artifactstores3location-ap8i1gybzyuw \
-    --region $region --capabilities 'CAPABILITY_NAMED_IAM' --output json --profile here
+# aws cloudformation create-stack \
+#     --stack-name notes-backend \
+#     --template-url https://s3.amazonaws.com/notes-artifactstores3location-ap8i1gybzyuw/backend.yml \
+#     --parameters ParameterKey=S3Artifact,ParameterValue=notes-artifactstores3location-ap8i1gybzyuw \
+#     --region $region --capabilities 'CAPABILITY_NAMED_IAM' --output json --profile here
 
-#./stackstatus.sh --region us-east-1 --watch --stack-name notes-backend --profile here
+./stackstatus.sh --region us-east-1 --watch --stack-name notes-backend --profile here
 
-# After cloudformation is deployed run the cognito crap
-
-sleep 90
+# After cloudformation is deployed run the cognito stuff
+secs=$((90))
+while [ $secs -gt 0 ]; do
+   echo -ne "$secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
 
 cd .. 
 python -u get_cf.py
